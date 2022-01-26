3230dc1ab8dd32442ab1ce45e7743e5e352fe69f
lucasnomi <edileniser@gmail.com>
2022-01-26 14:12:47 -0300

    feat: criar função para aparecer o jogo da memória

---
+++ js/forgot-password.js
@@ -0,0 +1,5 @@
+function showmemorygame(){
+    document.getElementById("div_memorygame").style.display = 'flex';
+    document.getElementById("div_form_login").style.display = 'none';
+    document.getElementById("div_form_profilepic").style.display = 'none';
+}
