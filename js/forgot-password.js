732d02acd3e7fe34a1c29ba0762c39259cecb23c
lucasnomi <edileniser@gmail.com>
2022-01-28 17:33:27 -0300

    fix: corrigido o nome de jogo da velha para memory game

--- js/forgot-password.js
+++ js/forgot-password.js
@@ -1,5 +1,5 @@
 function showmemorygame(){
-    document.getElementById("jogo-velha").style.display = 'grid';
+    document.getElementById("memory-game").style.display = 'grid';
     document.getElementById("form-login").style.display = 'none';
     document.getElementById("form-profilepic").style.display = 'none';
 }
